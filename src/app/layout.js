31bfd55941c3aa385088a41ace5bb9f96822bace
tanitadimitrovaa-sketch <tanitadimitrovaa@gmail.com>
2026-07-08 14:38:28 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -115,6 +116,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         {children}
+         <GoogleAnalytics gaId="G-SVB3QCT1TT" />
       </body>
     </html>
   );
